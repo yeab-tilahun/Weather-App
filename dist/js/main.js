810da22be2e5a4d9d3a0f7240f1a78ba0ddab783
Yeab-tilahun <yeabsiratilahun78@gmail.com>
2023-10-03 21:24:07 +0300

    fix: removed unnecessary code

--- dist/js/main.js
+++ dist/js/main.js
@@ -173,7 +173,6 @@ const submitEventLocation = async (event) => {
 
 const updateDataAndDisplay = async (locationObj) => {
     const weatherJson = await getWeatherFromCoords(locationObj)
-    console.log(weatherJson)
     if (weatherJson) updateDisplay(weatherJson, locationObj)
 }
 
